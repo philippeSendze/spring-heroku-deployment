076e18687bcb866f15a6f996502130283abd55ee
Philippe Sendze <sendze.philippe@gmail.com>
2022-02-07 16:27:04 +0100

    Initial commit

---
+++ src/main/java/com/festus/demo/DeploiementApplication.java
@@ -0,0 +1,29 @@
+package com.festus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DeploiementApplication {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world";
+	}
+
+	@GetMapping("/{input}")
+	public String getMessage(@PathVariable String input){
+		return "Hello" + input;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploiementApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/festus/demo/DeploiementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploiementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
